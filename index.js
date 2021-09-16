1c4241185f09f1f03e9b668aa952b0ebacf83e4f
domen <domen.baler@gmail.com>
2021-09-16 16:08:54 +0200

    ready for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
 morgan.token('body', (req, res) => { return JSON.stringify(req.body)})
 app.use(morgan( (tokens, req, res) => {
     return [
@@ -111,7 +114,7 @@ app.use(unknownEndpoin)
 
 
 
-const PORT = 3001
+const PORT =  process.env.PORT ||3001
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`)
 })
